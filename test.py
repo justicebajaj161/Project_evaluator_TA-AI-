75933add816b377370f69a931005a14bd11d5bfa
Coding Ninjas <codingninjas@Codings-MacBook-Pro.local>
2025-04-16 23:16:00 +0530

    third-commit

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests
 
 url = "http://localhost:8000/analyze-project"
-file_path = "Restaurantsolution.zip"
+# file_path = "Restaurantsolution.zip"
 # file_path = "Restaurantsolution2.zip"
 # file_path = "destinationsolution.zip"
-# file_path = "contactbook.zip"
+file_path = "contactbook.zip"
 
 with open(file_path, "rb") as f:
     files = {"zip_file": (file_path, f)}
